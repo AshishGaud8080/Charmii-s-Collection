d6eaac234796cc16eafb16efc5fcca028fd2ef96
AshishGaud8080 <ashishmgaud8080@gmail.com>
2024-06-17 23:05:29 +0530

    Frontend Project

---
+++ react/src/config/navigationMenu.js
@@ -0,0 +1,136 @@
+export const navigation = {
+    categories: [
+      {
+        id: 'Home',
+        name: 'Home',
+        featured: [
+          {
+            name: 'New Arrivals',
+            href: '/',
+            imageSrc: 'https://m.media-amazon.com/images/I/711H+Uq+8SL._SY625_.jpg',
+            imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+          },
+          {
+            name: 'Traditional Necklace',
+            href: '/',
+            imageSrc: 'https://m.media-amazon.com/images/I/81GOtF13cZL._SY695_.jpg',
+            imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+          },
+        ],
+        sections: [
+          // {
+          //   id: 'clothing',
+          //   name: 'Clothing',
+          //   items: [
+          //     { name: 'Tops', id:"top", href: `{women/clothing/tops}` },
+          //     { name: 'Dresses', id:"women_dress", href: '#' },
+          //     { name: 'Women Jeans', id: 'women_jeans' },
+          //     { name: 'Lengha Choli', id: 'lengha_choli' },
+          //     { name: 'Sweaters', id: 'sweater' },
+          //     { name: 'T-Shirts', id: 't-shirt' },
+          //     { name: 'Jackets', id: 'jacket' },
+          //     { name: 'Gouns', id: 'gouns' },
+          //     { name: 'Sarees', id: 'saree' },
+          //     { name: 'Kurtas', id: 'kurtas' },
+          //   ],
+          // },
+          // {
+          //   id: 'accessories',
+          //   name: 'Accessories',
+          //   items: [
+          //     { name: 'Watches', id: 'watch' },
+          //     { name: 'Wallets', id: 'wallet' },
+          //     { name: 'Bags', id: 'bag' },
+          //     { name: 'Sunglasses', id: 'sunglasse' },
+          //     { name: 'Hats', id: 'hat' },
+          //     { name: 'Belts', id: 'belt' },
+          //   ],
+          // },
+          // {
+          //   id: 'brands',
+          //   name: 'Brands',
+          //   items: [
+          //     { name: 'Full Nelson', id: '#' },
+          //     { name: 'My Way', id: '#' },
+          //     { name: 'Re-Arranged', id: '#' },
+          //     { name: 'Counterfeit', id: '#' },
+          //     { name: 'Significant Other', id: '#' },
+          //   ],
+          // },
+        ],
+      },
+      {
+        id: 'Shop',
+        name: 'Shop',
+        featured: [
+          {
+            name: 'New Arrivals',
+            id: '#',
+            imageSrc: 'https://m.media-amazon.com/images/I/71AzazQb6hL._SY625_.jpg',
+            imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+          },
+          {
+            name: 'Traditional Necklace',
+            id: '#',
+            imageSrc: 'https://m.media-amazon.com/images/I/81vLw-6u5NL._SY695_.jpg',
+            imageAlt:
+              'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+          },
+        ],
+        sections: [
+          {
+            id: 'All Products',
+            name: 'All Products',
+            items: [
+               { name: 'Earring', id: 'Earring' },       
+               { name: 'Ring', id: 'Ring' },
+               { name: 'Necklaces', id: 'Necklaces' },
+              // { name: 'Sweaters', id: '#' },
+              // { name: 'T-Shirts', id: 't-shirt' },
+              // { name: 'Jackets', id: '#' },
+              // { name: 'Activewear', id: '#' },
+              
+            ],
+          },
+          {
+            id: 'Glossaries',
+            name: 'Glossaries',
+            items: [
+               { name: 'Bangle', id: '#' },   
+               { name: 'Diamond', id: '#' },
+               { name: 'Gold', id: '#' },
+              // { name: 'Sunglasses', id: '#' },
+              // { name: 'Hats', id: '#' },
+              // { name: 'Belts', id: '#' },
+            ],
+          },
+          {
+            id: 'Custom Glossaries',
+            name: 'Custom Glossaries',
+            items: [
+               { name: 'Diamonds', id: '#' }, 
+               { name: 'Gold', id: '#' },
+               { name: 'Silver', id: '#' },
+              // { name: 'Sunglasses', id: '#' },
+              // { name: 'Hats', id: '#' },
+              // { name: 'Belts', id: '#' },
+            ],
+          },
+          // {
+          //   id: 'brands',
+          //   name: 'Brands',
+          //   items: [
+          //     { name: 'Tanishk', id: '#' },
+          //     { name: 'Bajaj Jwellers', id: '#' },
+          //     { name: 'Kalyan', id: '#' },
+          //     { name: 'Malbar', id: '#' },
+          //   ],
+          // },
+        ],
+      },
+    ],
+    pages: [
+      { name: 'Admin', id: '/' },
+      { name: 'Stores', id: '/' },
+    ],
+  }
